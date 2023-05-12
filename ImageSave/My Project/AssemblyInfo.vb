c9480f308d975590c960d4b9761591b7cfb89c69
fanqi <fanqi@DESKTOP-EO60T56>
2023-05-12 12:52:58 +0800

    更新产品版本号

--- ImageSave/My Project/AssemblyInfo.vb
+++ ImageSave/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 方法是按如下所示使用“*”:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.9.3.02")>
-<Assembly: AssemblyFileVersion("0.9.3.02")>
+<Assembly: AssemblyVersion("0.9.3.03")>
+<Assembly: AssemblyFileVersion("0.9.3.03")>
